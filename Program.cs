6f77fd87dae2461613df1681aa210671970edc5f
ashaelizabethjoseph <ashacep38@gmail.com>
2025-01-05 20:31:16 +0530

    swagger configured

--- Program.cs
+++ Program.cs
@@ -28,7 +28,22 @@ namespace EmployeeManagement
             builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 
             builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
+
+            if(app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI( c=> 
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee Management API V1");
+                    c.RoutePrefix = string.Empty;
+                });
+            }
             app.UseCors("MyCors");
 
             app.MapGet("/", () => "Hello World!");
